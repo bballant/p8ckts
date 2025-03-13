fedf450afccf384e7c99d9b8c4548f87ab19af26
Brian Ballantine <bballantine@earnestresearch.com>
2025-03-13 15:14:38 -0400

    added randomizer to ts-land

---
+++ randomizer/src/main.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+// for running as npx package after building
+
+const getRandomElement = (arr: string[]): string | undefined => {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+};
+
+console.log(process.argv);
+
+if (process.argv.length < 3) {
+    console.error("Please provide a list of strings.");
+    process.exit(1);
+}
+
+const inputStrings = process.argv.slice(2);
+console.log(getRandomElement(inputStrings));
